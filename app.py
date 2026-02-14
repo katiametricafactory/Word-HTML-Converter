f37510c8604b360f64bec701164005d050516c0d
Katia del Río <111348156+katiametricafactory@users.noreply.github.com>
2026-02-14 13:12:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,31 +7,29 @@ st.set_page_config(layout="wide")
 st.title("Word → HTML Converter")
 
 uploaded_file = st.file_uploader(
-    "Sube un archivo .doc o .docx",
-    type=["doc", "docx"]
+    "Sube un archivo .docx",
+    type=["docx"]
 )
 
 if uploaded_file:
 
-    # Generar HTML inicial solo una vez por archivo
     if "html_content" not in st.session_state:
         st.session_state.html_content = convert_word_to_html(uploaded_file)
 
     col1, col2 = st.columns(2)
 
     # =========================
-    # COLUMNA IZQUIERDA (EDITOR)
+    # COLUMNA IZQUIERDA
     # =========================
     with col1:
         st.subheader("Código HTML (editable)")
 
         st.session_state.html_content = st.text_area(
-            "Modifica el HTML si lo necesitas. Tienes que dar clic en la parte derecha para ver el cambio",
+            "Modifica el HTML si lo necesitas",
             value=st.session_state.html_content,
             height=600
         )
 
-        # Botón copiar (seguro con base64)
         encoded_html = base64.b64encode(
             st.session_state.html_content.encode()
         ).decode()
@@ -54,7 +52,7 @@ if uploaded_file:
         components.html(copy_button, height=80)
 
     # =========================
-    # COLUMNA DERECHA (PREVIEW)
+    # COLUMNA DERECHA
     # =========================
     with col2:
         st.subheader("Vista previa en tiempo real")
